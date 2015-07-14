390b9c961c4dc702b4494d4c395edc2df03d5927
Terence Lee <hone02@gmail.com>
2015-07-14 16:08:59 -0400

    pull config into it's own file

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 'use strict';
 
 let pkg = require('./package.json');
-
-const TOPIC = 'static';
-const CONFIG_FILE = 'static.json';
+let config = require("./lib/config");
 
 exports.topic = {
-  name: TOPIC,
+  name: config.TOPIC,
   description: pkg.description,
 };
 
 exports.commands = [
-  require('./lib/commands/init.js')(TOPIC, CONFIG_FILE),
-  require('./lib/commands/deploy.js')(TOPIC, CONFIG_FILE)
+  require('./lib/commands/init.js'),
+  require('./lib/commands/deploy.js')
 ];
