a51ce6fe159e0116a9a558fe61cb42cfbb55cebd
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-01-03 10:35:19 -0800

    fix topics export

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 let pkg = require('./package.json');
 let config = require("./lib/config");
 
-exports.topic = {
-  name: config.TOPIC,
+exports.topics = [{
+  name: config.topic,
   description: pkg.description,
-};
+}];
 
 exports.commands = [
   require('./lib/commands/init.js'),
